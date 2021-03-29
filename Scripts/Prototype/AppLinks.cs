977205c84c3860c6d34f90700710ec2d077c4ec3
mkolobov <mkolobov@playflock.com>
2021-03-29 21:02:33 +0300

    Remove cook dependencies

--- Scripts/Prototype/AppLinks.cs
+++
@@ -1,9 +0,0 @@
-﻿using RH.Utilities.SceneManagement;
-using RH.Utilities.Singleton;
-using UnityEngine;
-
-public class AppLinks : MonoSingleton<AppLinks>
-{
-    public Transform WindowsParent;
-    public Loader Loader;
-}
